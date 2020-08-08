b05227b2c5fc407691357f3fa30acbc02fbb2f59
sgldts <1033465081@qq.com>
2020-08-08 16:02:27 +0800

    Gitee

---
+++ src/main/java/com/sgldts/community/controller/GiteeController.java
@@ -0,0 +1,62 @@
+package com.sgldts.community.controller;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.sgldts.community.dto.GiteeUser;
+import com.sgldts.community.utils.GiteeHttpClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.util.UUID;
+
+/**
+ * @author herry
+ * @create 2020-08-08-15:21
+ */
+@Controller
+public class GiteeController {
+
+    @Value("${gitee.oauth.clientid}")
+    private String CLIENTID;
+
+    @Value("${gitee.oauth.clientsecret}")
+    private String CLIENTSECRET;
+
+    @Value("${gitee.oauth.callback}")
+    private String URL;
+
+    /**
+     * 授权回调
+     */
+    @GetMapping(value = "/callback")
+    public String qqCallback(HttpServletRequest request) throws Exception {
+        // 得到Authorization Code
+        String code = request.getParameter("code");
+
+        // Step2：通过Authorization Code获取Access Token
+        String url = "https://gitee.com/oauth/token?grant_type=authorization_code" +
+                "&client_id=" + CLIENTID +
+                "&client_secret=" + CLIENTSECRET +
+                "&code=" + code +
+                "&redirect_uri=" + URL;
+        JSONObject accessTokenJson = GiteeHttpClient.getAccessToken(url);
+
+        // Step3: 获取用户信息
+        url = "https://gitee.com/api/v5/user?access_token=" + accessTokenJson.get("access_token");
+        JSONObject jsonObject = GiteeHttpClient.getUserInfo(url);
+        /**
+         * 获取到用户信息之后，就该写你自己的业务逻辑了
+         */
+        System.out.println(jsonObject.toString());
+        String string = jsonObject.toString();
+        GiteeUser giteeUser = JSON.parseObject(string, GiteeUser.class);
+        System.out.println(giteeUser);
+        return "index";
+    }
+
+}
---
+++ src/main/java/com/sgldts/community/dto/GiteeUser.java
@@ -0,0 +1,13 @@
+package com.sgldts.community.dto;
+
+import lombok.Data;
+
+/**
+ * @author herry
+ * @create 2020-08-08-15:47
+ */
+@Data
+public class GiteeUser {
+    private String name;
+    private Long id;
+}
